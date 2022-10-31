9912d7d00e22279c874274bc6910c39afa780686
michurinnn <michurinnn@gmail.com>
2022-11-01 01:02:05 +0300

    Добавлена документуия к классу, исправлены баги

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
+// Класс создаёт екран с корневым виджетом Scaffold, используется в практической работе 4-2 на курсе Surf. 
+// Параметр resizeToAvoidBottomInset: false для демонстрации поведения содержимого Scaffold при изменении размера екрана (например, при появлении клавиатуры)
 class SightListScreen extends StatefulWidget {
   const SightListScreen({Key? key}) : super(key: key);
 
@@ -13,7 +15,7 @@ class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false, // Чтобы содержимое Scaffold не смещалось при изменении размера екрана (например, при появлении клавиатуры
+      resizeToAvoidBottomInset: false, // Чтобы содержимое Scaffold не смещалось при изменении размера екрана (например, при появлении клавиатуры)
       body: Center(child: Text("Hello!")),
     );
   }
