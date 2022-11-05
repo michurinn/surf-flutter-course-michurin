06ac339083f4323525b22676b961244c16646ba9
michurinnn <michurinnn@gmail.com>
2022-11-05 23:05:17 +0300

    2.

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:places/mocks.dart';
+import 'package:places/ui/screen/sight_card.dart';
 
 // Екран списка мест
 class SightListScreen extends StatefulWidget {
   const SightListScreen({Key? key}) : super(key: key);
-  
 
   @override
   State<SightListScreen> createState() => _SightListScreenState();
@@ -13,19 +14,35 @@ class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        toolbarHeight: 100,        
-          backgroundColor: ThemeData.light().scaffoldBackgroundColor,
-          elevation: 0,
-          
-          title: const Text("Список\nинтересных мест",
-                style: const TextStyle(color: const Color(0xFF3B3E5B),
-                fontFamily: "Roboto",
-                fontSize: 32,
-                fontWeight: FontWeight.w700),
-                softWrap: true,),                
-          ),
+        toolbarHeight: 120,
+        backgroundColor: ThemeData.light().scaffoldBackgroundColor,
+        elevation: 0,
+        title: const Text(
+          "Список\nинтересных мест",
+          style: TextStyle(
+              color: Color(0xFF3B3E5B),
+              fontFamily: "Roboto",
+              fontSize: 32,
+              fontWeight: FontWeight.w700),
+          softWrap: true,
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SightCard(sight: mocks[0]),
+            const SizedBox(
+              height: 20,
+            ),
+            SightCard(sight: mocks[1]),
+            const SizedBox(
+              height: 20,
+            ),
+            SightCard(sight: mocks[2]),
+          ],
+        ),
+      ),
       resizeToAvoidBottomInset:
           false, // Чтобы содержимое Scaffold не смещалось при изменении размера екрана (например, при появлении клавиатуры)
     );
