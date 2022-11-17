a9eb69c1a17b435580d29087824b6c5720cb092d
michurinnn <michurinnn@gmail.com>
2022-11-18 00:00:29 +0300

    4-8-1 Homework

--- lib/domain/sight.dart
+++ lib/domain/sight.dart
@@ -1,11 +1,16 @@
 // Класс места, содержит информацию об интересном месте
-class Sight 
-{
-   final String name;
-   final double lat;
-   final double lon;
-   final String details;
-   final String type;
+class Sight {
+  final String name;
+  final double lat;
+  final double lon;
+  final String details;
+  final String type;
 
-  const Sight(this.name, this.lat, this.lon, this.details, this.type,);
+  const Sight(
+    this.name,
+    this.lat,
+    this.lon,
+    this.details,
+    this.type,
+  );
 }
