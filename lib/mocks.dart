142a7af6405d0c094b7a22cc9d60a8fdabe26190
michurinnn <michurinnn@gmail.com>
2022-11-26 19:32:30 +0300

    4-10 images

--- lib/mocks.dart
+++ lib/mocks.dart
@@ -4,9 +4,14 @@ import 'package:places/res/AppStrings.dart';
 //Заглушки с местами
 final List mocks = [
   const Sight(AppStrings.sight0name, 59.87717719312971, 29.908010865005355,
-      AppStrings.sight0subscr, AppStrings.sightType0,'assets/olgin.jpg'),
+      AppStrings.sight0subscr, AppStrings.sightType0, 'assets/olgin.jpg'),
   const Sight(AppStrings.sight1name, 59.974382117593564, 29.716890776684536,
-      AppStrings.sight1subscr, AppStrings.sightType1,'assets/fort.jpg'),
-  const Sight(AppStrings.sight2name, 60.71573297869801, 28.729246819869616,
-      AppStrings.sight2subscr, AppStrings.sightType1,'assets/vyborgskiy-zamok.jpg'),
+      AppStrings.sight1subscr, AppStrings.sightType1, 'assets/fort.jpg'),
+  const Sight(
+      AppStrings.sight2name,
+      60.71573297869801,
+      28.729246819869616,
+      AppStrings.sight2subscr,
+      AppStrings.sightType1,
+      'assets/vyborgskiy-zamok.jpg'),
 ];
